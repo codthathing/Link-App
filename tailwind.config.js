3b9f0ec147ab27b47369af664a504d4d54c95d44
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-12-06 16:57:29 +0100

    updated the shadow of the inputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ export default {
         "[0.15px]": "0.15px",
       },
       boxShadow: {
-        "inset-white": "0 0 0px 1000px transparent inset",
+        "inset": "0 0 0px 1000px inset",
       },
     },
   },
