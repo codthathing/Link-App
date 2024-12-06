76e7290d70b57bd41bd72cf34f947637a9f37d66
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-12-06 16:24:52 +0100

    adjusted the outline of the inputs

--- src/components/(sign-in)/SignInInput.jsx
+++ src/components/(sign-in)/SignInInput.jsx
@@ -7,7 +7,7 @@ const SignInInput = ({label, id, type, name, value, onChange, placeholder, error
       <label htmlFor={id} className="mb-1 md:mb-2 text-grey-dark text-xs md:text-lg lg:text-base">{label}</label>
       <main className="border border-grey-semilight flex items-center gap-x-2 p-3 md:p-5 rounded-md md:rounded-lg">
         <img src={type === "email" ? email_icon : lock_icon} className="w-3 md:w-8 lg:w-5" alt={`${type.toUpperCase()} ICON`} />
-        <input type={type} id={id} name={name} value={value} onChange={onChange} className="w-full text-grey-dark bg-transparent text-[8px] md:text-base lg:text-xs" placeholder={placeholder} />
+        <input type={type} id={id} name={name} value={value} onChange={onChange} className="w-full outline-none text-grey-dark bg-transparent text-[8px] md:text-base lg:text-xs" placeholder={placeholder} />
         <span className="justify-self-end whitespace-nowrap hidden text-customRed text-[8px] md:text-base lg:text-xs">{errorMessage}</span>
       </main>
     </div>
