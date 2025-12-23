e5495a11cf77c2bc98eda14c8001d33ebae33bb2
Akinwunmi <akinwunmiolusegun277@gmail.com>
2025-12-23 16:39:02 +0100

    feat: added cloudinary to the project

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -8,6 +8,9 @@ import { useContext, useRef, useState } from "react";
 import { NavigateContext } from "../../services/NavigateProvider";
 import { UserContext } from "../../pages/UserPage";
 import { supabase } from "../../database/supabaseClient";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 const ProfilePage = ({ className }) => {
   const { user: { details } } = useContext(NavigateContext);
@@ -15,48 +18,74 @@ const ProfilePage = ({ className }) => {
 
   const inputRef = useRef();
 
-  const [profileInput, setProfileInput] = useState({ profile_picture: "", first_name: "", last_name: "", email: "" });
+  const [profileInput, setProfileInput] = useState({ first_name: "", last_name: "" });
   const handleProfileInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setProfileInput({ ...profileInput, [name]: value });
   };
 
-  const [errorResponse, setErrorResponse] = useState({ first_name: "", last_name: "" });
+  const uploadToCloudinary = async (file) => {
+    const formData = new FormData();
+    formData.append("files", file);
+    formData.append("upload_preset", "link_app_unsigned_preset");
+    formData.append("allowed_formats", "jpg,png");
+    let public_id;
+
+    try {
+      const response = await fetch("https://api.cloudinary.com/v1_1/doy5yq79f/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) throw new Error(`Upload Error: ${response.statusText}`);
+
+      const data = await response.json();
+      public_id = data.public_id;
+    } catch (err) {
+      console.error(err);
+    }
+
+    const cld = new Cloudinary({ cloud: { cloudName: "doy5yq79f" } });
+    const image = cld.image(public_id).format("auto").quality("auto").resize(fill().gravity(focusOn(FocusOn.face())).width(1024).height(1024));
+
+    updateUserDatabase({ user_profile: image });
+  };
 
   const updateUserDatabase = async (data) => {
     try {
-      const { error } = await supabase.from('users').update(data).eq('id', details.id).select();
-      
+      const { error } = await supabase.from("users").update(data).eq("id", details.id).select();
+
       if (error) throw Error;
     } catch (err) {
       console.error(err);
-    };
-  }
+    }
+  };
+
+  const [errorResponse, setErrorResponse] = useState({ first_name: "", last_name: "" });
 
   const updateProfileDetails = async () => {
     const updatedResponse = { first_name: "", last_name: "" };
     let notisMessage = "";
-    if (profileInput.first_name || profileInput.last_name || profileInput.email) {
+    if (profileInput.first_name || profileInput.last_name) {
       if ((!profileInput.first_name && !details.firstname) || (!profileInput.last_name && !details.lastname)) {
         if (!profileInput.first_name && !details.firstname) updatedResponse.first_name = "Can't be empty";
         if (!profileInput.last_name && !details.lastname) updatedResponse.last_name = "Can't be empty";
-      };
-      const updateFisrtName = (profileInput.first_name && (profileInput.last_name || details.lastname));
-      const updateLastName = (profileInput.last_name && (profileInput.first_name || details.firstname));
-      const updateEmail = profileInput.email && (profileInput.email !== details.email);
-      if (updateFisrtName || updateLastName || updateEmail) {
-        const updatedDetails = { ...(updateFisrtName && { user_firstname: profileInput.first_name }), ...(updateLastName && { user_lastname: profileInput.last_name }), ...(updateEmail && { user_email: profileInput.email }) };
+      }
+      const updateFisrtName = profileInput.first_name && (profileInput.last_name || details.lastname);
+      const updateLastName = profileInput.last_name && (profileInput.first_name || details.firstname);
+      if (updateFisrtName || updateLastName) {
+        const updatedDetails = { ...(updateFisrtName && { user_firstname: profileInput.first_name }), ...(updateLastName && { user_lastname: profileInput.last_name }) };
         updateUserDatabase(updatedDetails);
-        setProfileInput({ ...profileInput, ...(updateFisrtName && { first_name: "" }), ...(updateLastName && { last_name: "" }), ...(updateEmail && { email: "" }) });
+        setProfileInput({ ...profileInput, ...(updateFisrtName && { first_name: "" }), ...(updateLastName && { last_name: "" }) });
         notisMessage = "Your changes have been successfully saved!";
-      };
-    };
+      }
+    }
     setSavedMessage(notisMessage);
     setTimeout(() => {
       if (notisMessage) {
         setSavedMessage("");
-      };
+      }
     }, 2000);
     setErrorResponse(updatedResponse);
   };
@@ -68,13 +97,7 @@ const ProfilePage = ({ className }) => {
         <div className="bg-grey-light mt-3 mb-2 p-2 md:p-6 lg:p-4 md:mt-8 md:mb-6 lg:mt-6 lg:mb-4 md:grid md:grid-cols-3 md:gap-x-6 lg:gap-x-44 md:items-center rounded-lg">
           <span className="text-xs md:text-xl lg:text-base text-grey-normal">Profile picture</span>
           <div key={details.profile} style={{ ...(details.profile && { backgroundImage: `url(${details.profile})` }) }} onClick={() => inputRef.current.click()} className={`${details.profile ? "" : "bg-purple-light"} bg-cover bg-no-repeat w-2/4 h-32 md:w-full md:h-60 lg:h-40 rounded-2xl my-1 md:m-0 flex flex-col gap-y-2 items-center justify-center cursor-pointer`}>
-            <input type="file" name="profile_picture" className="p-0" accept="image/png, image/jpeg" ref={inputRef} hidden onChange={({ target: { files } }) => {
-              if (files && files[0]) {
-                const reader = new FileReader();
-                reader.onload = (e) => updateUserDatabase({ image_url: e.target.result });
-                reader.readAsDataURL(files[0]);
-              }
-            }} />
+            <input type="file" className="p-0" accept="image/png, image/jpeg" ref={inputRef} hidden onChange={({ target: { files } }) => uploadToCloudinary(files[0])} />
             <img src={details.profile ? image_white_icon : image_purple_icon} className="w-6 md:w-10" alt="IMAGE PURPLE ICON" />
             <span className={`text-xs md:text-xl lg:text-base ${details.profile ? "text-white" : "text-purple-custom"}`}>+ Upload Image</span>
           </div>
@@ -83,12 +106,11 @@ const ProfilePage = ({ className }) => {
         <div className="bg-grey-light p-2 md:p-6 lg:p-4 flex flex-col gap-y-1 md:gap-y-4 lg:gap-y-3 rounded-lg">
           <ProfileInput id={"profile-first-name"} label={"First name*"} type={"text"} name={"first_name"} placeholder={"e.g. John"} value={profileInput.first_name} onChange={handleProfileInput} errorMessage={errorResponse.first_name} />
           <ProfileInput id={"profile-last-name"} label={"Last name*"} type={"text"} name={"last_name"} placeholder={"e.g. Doe"} value={profileInput.last_name} onChange={handleProfileInput} errorMessage={errorResponse.last_name} />
-          {/* <ProfileInput id={"profile-email"} label={"Email"} type={"email"} name={"email"} placeholder={"e.g. email@example.com"} value={profileInput.email} onChange={handleProfileInput} /> */}
         </div>
       </main>
-      <UserButton buttonFunction={updateProfileDetails} changeColor={profileInput.profile_picture || profileInput.first_name || profileInput.last_name || profileInput.email} className={"absolute"} />
+      <UserButton buttonFunction={updateProfileDetails} changeColor={profileInput.first_name || profileInput.last_name} className={"absolute"} />
     </UserSection>
   );
 };
 
-export default ProfilePage
+export default ProfilePage;
